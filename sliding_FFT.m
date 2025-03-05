06a3866a1d3c6e75020a15d8a9c5c344557f70e6
jawshan <jawshan@gmail.com>
2025-03-05 15:35:55 -0800

    Slidingwindow FFT in matlab

--- sliding_FFT.m
+++ sliding_FFT.m
@@ -1,9 +1,5 @@
-clear
-clc
 
-% read Data
 directory='/Users/shatil/Library/CloudStorage/Box-Box/VEDB_IN/Good_data/angular_vel_data';
-%folder_name= '2022_02_09_13_40_13';
 folder= ["2022_02_09_13_40_13", "2022_06_24_11_41_06", "2022_06_24_11_49_03", "2022_06_24_14_26_28", "2022_09_15_15_25_58", "2022_09_19_11_27_04", "2022_10_06_15_51_11"];
 
 for i = 1:7
@@ -12,8 +8,6 @@ for i = 1:7
         N=int2str(n);
         filename = strcat(directory, '/',folder_name, '_', 'ang_vel_', N, '.xlsx');
         filenameT=strcat(directory,'/',folder_name, '_', 'timestamp.xlsx');
-        %% prepping x and y axis for FFT
-
         timestamp = readtable(filenameT);
         Time=table2array(timestamp);
         Time = Time(2:end);
@@ -23,37 +17,29 @@ for i = 1:7
         
         acceptable_index = length(ang_vel);
         
-
-%         Time_cap=find(Time>299.999 & Time<300.006);
-%         Time=Time(1:Time_cap(1));
-%         ang_vel=ang_vel(1:Time_cap(1));
-%         acceptable_index = Time_cap(1);
-%         
-        %% calculating sampling frequency
-
+        %sampling frequency
         Fs=floor(acceptable_index/Time(acceptable_index)); 
 
-        %% Section 1.1: FFT for angular velocity 10s long window sliding with 4s window
-        z=zeros([10*Fs 1]); %6s window
+        %% FFT for angular velocity 
+        z=zeros([6*Fs 1]); %6s window
         window_size = 6; %in seconds
         sliding_width= 1; %in seconds
         counter=floor(((length(ang_vel) -(window_size*Fs))/(sliding_width*Fs)))+1;
         dataset_complex_magnitude =[];
         dataset_absFFT = [];
         for N=1:counter
-            x_start=floor((N-1)*(sliding_width*Fs)+1); %4s second of sliding 4*Fs
+            x_start=floor((N-1)*(sliding_width*Fs)+1); 
             
-            x_end=floor((N-1)*(sliding_width*Fs)+(window_size*Fs));% 10s window length 10*Fs
-        %error management
+            x_end=floor((N-1)*(sliding_width*Fs)+(window_size*Fs));
                 if x_end > length(ang_vel)
                     x_end = length(ang_vel);
                 end
         %% windowing the signal
             z=ang_vel(x_start:x_end);
-            x = 0:length(z)-1; % number of collected data points 
-            L = length(x);     % the length of the collected data points.
-            samp_rate = Fs;    % the sampling rate of the eeg data file
-            freqx = 0:samp_rate/L:(samp_rate-samp_rate/L);  % the signal frequency.            
+            x = 0:length(z)-1; 
+            L = length(x);     
+            samp_rate = Fs;    
+            freqx = 0:samp_rate/L:(samp_rate-samp_rate/L);            
             window_fft = fft(z);
         %% single sided FFT, amplitude of the real value
             P2 = abs(window_fft/L);
@@ -77,20 +63,18 @@ for i = 1:7
     
     %% Finding the window of the max and first head shake
     
-    max_x_start=floor((i_col_max)*(sliding_width*Fs)); %4s second of sliding 4*Fs
-    max_x_end=floor((i_col_max)*(sliding_width*Fs)+(window_size*Fs));% 6s window length 10*Fs
+    max_x_start=floor((i_col_max)*(sliding_width*Fs)); 
+    max_x_end=floor((i_col_max)*(sliding_width*Fs)+(window_size*Fs));
     
-    shake_x_start=floor((i_col_shake)*(sliding_width*Fs)); %4s second of sliding 4*Fs
-    shake_x_end=floor((i_col_shake)*(sliding_width*Fs)+(window_size*Fs));% 6s window length 10*Fs
+    shake_x_start=floor((i_col_shake)*(sliding_width*Fs)); 
+    shake_x_end=floor((i_col_shake)*(sliding_width*Fs)+(window_size*Fs));
     
     max_window_start_time = Time(max_x_start);
     max_window_end_time = Time(max_x_end);
     
-    
     shake_window_start_time = Time(shake_x_start);
     number_shake_present=length(shake_window_start_time);
     shake_window_end_time = Time(shake_x_end);
-    
     
     HeadCal_Time_title = {folder_name n "Max Start Time" max_window_start_time "First Shake Start Time" shake_window_start_time(1) 
                           folder_name n "Max End Time" max_window_end_time "First Shake End Time" shake_window_end_time(1)};
